08edd86df5f978d61a2411c545e0cb8a556b61c6
jerry <jzhang12325@gmail.com>
2019-02-15 22:04:20 -0700

    Updates

---
+++ precache-manifest.7ae563b1dcd7fc9440e3420728d49ef6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "40db4dd3cf9a8f68bbf5",
+    "url": "/calc/static/css/main.03395196.chunk.css"
+  },
+  {
+    "revision": "40db4dd3cf9a8f68bbf5",
+    "url": "/calc/static/js/main.40db4dd3.chunk.js"
+  },
+  {
+    "revision": "d27d2e21452bbd409f11",
+    "url": "/calc/static/js/runtime~main.d27d2e21.js"
+  },
+  {
+    "revision": "0c17919a6e39fa9557f6",
+    "url": "/calc/static/js/2.0c17919a.chunk.js"
+  },
+  {
+    "revision": "039fc1ae2909b5042f8c998e2c353935",
+    "url": "/calc/index.html"
+  }
+];
